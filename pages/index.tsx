f9e80d7a3983f2bb1e63e64d5b091c317b8711d1
wloodar <wlodardev@gmail.com>
2022-09-02 04:18:03 +0200

    Initial project setup

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+    return (
+        <div className="w-full h-screen flex items-center justify-center">
+            <span className="text-2xl">Hello, from ELCO</span>
+        </div>
+    )
+}
+
+export default Home
