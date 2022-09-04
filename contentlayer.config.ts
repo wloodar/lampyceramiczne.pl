b7a6bcfcf79b648f058dff45afd766d7f8748ee3
wloodar <wlodardev@gmail.com>
2022-09-04 21:41:36 +0200

    Contentlayer for transfomring mdx into json and content dir

---
+++ contentlayer.config.ts
@@ -0,0 +1,26 @@
+import { defineDocumentType, makeSource } from 'contentlayer/source-files'
+
+export const LampPage = defineDocumentType(() => ({
+    name: 'Lamp',
+    filePathPattern: 'lamps/*.mdx',
+    fields: {
+        title: { type: 'string', required: true },
+        date: { type: 'string', required: true },
+    },
+    computedFields: {
+        slug: {
+            type: 'string',
+            resolve: doc => doc._raw.sourceFileName.replace('.mdx', ''),
+        },
+    },
+}))
+
+export default makeSource({
+    contentDirPath: 'content',
+    documentTypes: [LampPage],
+    mdx: {
+        remarkPlugins: [],
+        rehypePlugins: [],
+    },
+    disableImportAliasWarning: true,
+})
