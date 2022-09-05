812e1e516626c6d94f76f1760397debbdfbddbd4
wloodar <wlodardev@gmail.com>
2022-09-05 04:39:29 +0200

    noSSR Wrapper types fix

--- common/components/noSSRWrapper.tsx
+++ common/components/noSSRWrapper.tsx
@@ -1,7 +1,9 @@
 import dynamic from 'next/dynamic'
 import React from 'react'
 
-const NonSSRWrapper = props => <React.Fragment>{props.children}</React.Fragment>
+const NonSSRWrapper = ({ children }: { children: React.ReactNode }) => (
+    <React.Fragment>{children}</React.Fragment>
+)
 
 export default dynamic(() => Promise.resolve(NonSSRWrapper), {
     ssr: false,
