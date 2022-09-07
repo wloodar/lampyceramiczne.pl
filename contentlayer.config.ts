95a74ded972469da8d7306c96e5e693b96cd2c3b
wloodar <wlodardev@gmail.com>
2022-09-07 02:34:24 +0200

    Few content entries added

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,12 +1,25 @@
-import { defineDocumentType, makeSource } from 'contentlayer/source-files'
+import { defineDocumentType, defineNestedType, makeSource } from 'contentlayer/source-files'
+
+const Technical = defineNestedType(() => ({
+    name: 'technical',
+    fields: {
+        height: { type: 'string', required: false },    
+        widthShade: { type: 'string', required: false },
+    }
+}))
 
 export const LampPage = defineDocumentType(() => ({
     name: 'Lamp',
     filePathPattern: 'lamps/*.mdx',
     fields: {
         title: { type: 'string', required: true },
+        shortDescription: { type: 'string', required: false },
         date: { type: 'string', required: true },
         cover: { type: 'string', required: true },
+        technical: {
+            type: 'list',
+            of: Technical
+        }
     },
     computedFields: {
         slug: {
