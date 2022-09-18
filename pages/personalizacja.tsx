b1b23090d318ee9d3d59ddf34f93d8979f6e5724
wloodar <wlodardev@gmail.com>
2022-09-18 13:14:32 +0200

    Getting rid of unused pages

--- pages/personalizacja.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Personalization = () => {
-    return <div></div>
-}
-
-export default Personalization
--- pages/wspolpraca.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Collaboration = () => {
-    return <div>Collaboration</div>
-}
-
-export default Collaboration
